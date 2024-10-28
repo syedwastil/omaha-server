b6b5db2339a7d86c80187605ccc4eb286dc86448
Naeem <naaeemmalik21@gmail.com>
2024-10-28 18:41:32 +0500

    Seperated dev and production settings

---
+++ omaha-server/config/settings_dev.py
@@ -0,0 +1,18 @@
+# your_project/settings/development.py
+
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+print("In settings_dev.py")
+# Development database (SQLite)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Additional development-specific settings
