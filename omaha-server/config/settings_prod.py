7c4ad9cd8bad4db25682cc0aa27f938786720580
Abdur Rehman Wasti <mwasti.bee18seecs@seecs.edu.pk>
2024-10-29 11:31:40 +0500

    Production settings updated

---
+++ omaha-server/config/settings_prod.py
@@ -0,0 +1,18 @@
+# your_project/settings/development.py
+
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
